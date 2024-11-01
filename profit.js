1c244980873690931675e810921223ea011fa429
Gasana22 <114236235+Gasana22@users.noreply.github.com>
2024-11-01 23:38:08 +0300

    Add files via upload

---
+++ profit.js
@@ -0,0 +1,6 @@
+//function for finding the profit
+
+function profit(sell_cost,buy_cost,number_of_products){
+       var profit=(sell_cost-(buy_cost*number_of_products))
+       console.log("profit:", profit)
+} profit(500000,40000,5)
